e86cab8aa9854bd98c5d3ef086f471c31a519923
azad-ali786 <azad_ug@cse.nits.ac.in>
2021-04-15 02:56:38 +0530

    Cards done

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,11 +3,23 @@ import styled from 'styled-components'
 import {motion} from "framer-motion"
 const Game =({name,released,image})=> {
     return (
-        <div>
+        <StyledGame>
             <h3>{name}</h3>
             <h3>{released}</h3>
             <img src={image} alt={name}/>
-        </div>
+            </StyledGame>
     )
 };
+const StyledGame = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+  cursor: pointer;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 40vh;
+    object-fit: cover;
+  }`;
 export default Game;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,15 +16,27 @@ const Home = () => {
     return(
         <GameList>
             <h2>Upcoming games</h2>
+            <Games>
             { upcoming && (
                     upcoming.map((game) => (       
                            <Game name={game.name} released={game.released} id={game.id} key={game.id} image={game.background_image}/>
                         ) )
                 )}
+            </Games>
         </GameList>
     )
 };
 const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2{
+      padding: 5rem 0rem;
+  }
 `;
-const Games = styled(motion.div)``;
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 export default Home;
